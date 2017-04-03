0d71f3cea8368815128dfddaf51c1eda6bd6be5d
Mark Talbot <markjtalbot@gmail.com>
2017-04-03 19:44:59 -0400

    Basic hello world

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-const electron = require('electron');
+const electron      = require('electron');
+const app           = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
-console.log('hello world');
+app.on('ready', () => {
+    console.log('hello world');
+
+    window = new BrowserWindow({ height: 600, width: 800 });
+    window.loadURL(`file://${__dirname}/index.html`);
+});
